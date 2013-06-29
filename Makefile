45ea0deb0d9808f91059526272522cc9501140a6
Toni Spets <toni.spets@iki.fi>
2013-06-30 01:17:40 +0300

    WOL CnCNet 5 compatibility for PlayCnC

--- Makefile
+++ Makefile
@@ -1,14 +1,14 @@
 CC=i686-w64-mingw32-gcc
 CFLAGS=-std=c99 -O2 -s -Wall -fno-strict-aliasing
 WINDRES=i686-w64-mingw32-windres
-LIBS=-lws2_32 -liphlpapi
+LIBS=-lws2_32 -lmsvcrt
 REV=$(shell sh -c 'git rev-parse --short @{0}')
 
 all: wsock32
 
-wsock32: wsock32.c wsock32.def wsock32.rc.in thipx32.c
+wsock32: wsock32.c wsock32.def wsock32.rc.in
 	sed 's/__REV__/$(REV)/g' wsock32.rc.in | $(WINDRES) -O coff -o wsock32.rc.o
-	$(CC) $(CFLAGS) -Wl,--enable-stdcall-fixup -shared -s -o wsock32.dll wsock32.c thipx32.c wsock32.def wsock32.rc.o $(LIBS)
+	$(CC) $(CFLAGS) -nostdlib -Wl,--enable-stdcall-fixup -shared -s -o wsock32.dll wsock32.c wsock32.def wsock32.rc.o $(LIBS)
 
 clean:
 	rm -f wsock32.dll wsock32.rc.o
--- wsock32.c
+++ wsock32.c
@@ -16,8 +16,6 @@
 
 #include <winsock2.h>
 #include <windows.h>
-#include <wsipx.h>
-#include <iphlpapi.h>
 #include <stdbool.h>
 
 #ifdef _DEBUG
@@ -27,241 +25,117 @@
     #define dprintf(...)
 #endif
 
-#define CNCNET_PORT 8054
-#define MAX_BCAST   8
+BOOL WINAPI DllMainCRTStartup(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) { return TRUE; }
 
-static unsigned int ip = INADDR_ANY;
-static unsigned int bcast = INADDR_BROADCAST;
-static bool porthack = true;
-static bool compmode = false;
-static struct sockaddr_in bcast_list[MAX_BCAST];
+static u_long tunnel_ip = 0;
+static short port = 0;
+static int irc_socket = 0;
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+static char recv_buf[1024];
+static int recv_buf_pos = 0;
+static int recv_buf_len = sizeof recv_buf; // IRC message max length is 512 bytes, double allows splitting safely
+
+// does really bad heurestics to detect the IRC socket and then parse TunnelServ messages
+int WINAPI compat_recv(SOCKET s, char *buf, int len, int flags)
 {
-    if (fdwReason == DLL_PROCESS_ATTACH)
-    {
-        char buf[512];
-        if (GetEnvironmentVariable("CNCNET_NODES", buf, sizeof buf) > 0)
+    int ret = recv(s, buf, len, flags);
+
+    if (ret != -1 && len > 1) {
+        dprintf("recv(s=%d, buf=%p, len=%d, flags=%08X) -> %d (err: %d)\n", s, buf, len, flags, ret, WSAGetLastError());
+
+        if (strstr(buf, "TunnelServ"))
+            irc_socket = s;
+
+        if (s != irc_socket)
+            return ret;
+
+        if (recv_buf_pos + ret < recv_buf_len)
         {
-            dprintf("CnCNet 5 compatibility mode enabled:\n");
+            memcpy(recv_buf + recv_buf_pos, buf, ret);
+            recv_buf_pos += ret;
+            recv_buf[recv_buf_pos] = '\0';
 
-            int i = 0;
-            char *p = strtok(buf, " ");
-            while (p)
+            char *l = recv_buf;
+
+            for (int i = 0; i < recv_buf_pos; i++)
             {
-                char *ip = p;
-                char *port = strchr(p, ':');
-
-                if (port != NULL && port - ip < strlen(p))
+                if (recv_buf[i] == '\n')
                 {
-                    *port++ = '\0';
+                    recv_buf[i] = '\0';
 
-                    bcast_list[i].sin_family = AF_INET;
-                    bcast_list[i].sin_addr.s_addr = inet_addr(ip);
-                    bcast_list[i].sin_port = htons(atoi(port));
-
-                    if (ntohs(bcast_list[i].sin_port) != CNCNET_PORT)
+                    if (strncmp(l, ":TunnelServ!", 12) == 0)
                     {
-                        porthack = false;
+                        dprintf("Message: %s\n", l);
+
+                        char *last = strrchr(l, ' ');
+                        if (last && last - recv_buf > 1)
+                        {
+                            last++;
+                            tunnel_ip = inet_addr(last);
+                            if (tunnel_ip == 0xFFFFFFFF)
+                                tunnel_ip = 0;
+                            dprintf("Tunnel ip set to %08X\n", tunnel_ip);
+                        }
                     }
 
-                    dprintf("  %s:%d\n", inet_ntoa(bcast_list[i].sin_addr), ntohs(bcast_list[i].sin_port));
-
-                    i++;
+                    l = recv_buf + i + 1;
                 }
-
-                p = strtok(NULL, " ");
             }
 
-            dprintf("Port hack is %s.\n", porthack ? "enabled" : "disabled");
-
-            compmode = true;
-        }
-    }
-
-    return TRUE;
-}
-
-static void ipx2in(const struct sockaddr_ipx *from, struct sockaddr_in *to)
-{
-    memset(to, 0, sizeof *to);
-
-    to->sin_family = AF_INET;
-    memcpy(&to->sin_addr.s_addr, from->sa_nodenum, 4);
-    memcpy(&to->sin_port, from->sa_nodenum + 4, 2);
-
-    if (from->sa_nodenum[0] == -1 || from->sa_netnum[0] == -1)
-    {
-        to->sin_addr.s_addr = bcast;
-    }
-}
-
-static void in2ipx(const struct sockaddr_in *from, struct sockaddr_ipx *to)
-{
-    memset(to, 0, sizeof *to);
-    to->sa_family = AF_IPX;
-    *(DWORD *)&to->sa_netnum = 0xDEADBEEF;
-    memcpy(to->sa_nodenum, &from->sin_addr.s_addr, 4);
-    memcpy(to->sa_nodenum + 4, &from->sin_port, 2);
-}
-
-SOCKET WINAPI ipx_socket(int af, int type, int protocol)
-{
-    dprintf("socket(af=%08X, type=%08X, protocol=%08X)\n", af, type, protocol);
-
-    if (af == AF_IPX)
-    {
-        return socket(AF_INET, SOCK_DGRAM, 0);
-    }
-
-    return socket(af, type, protocol);
-}
-
-int WINAPI ipx_bind(SOCKET s, const struct sockaddr_ipx *name, int namelen)
-{
-    dprintf("bind(s=%d, name=%p, namelen=%d)\n", s, name, namelen);
-
-    if (((struct sockaddr_ipx *)name)->sa_family == AF_IPX)
-    {
-        DWORD ret;
-        ULONG adapters_size = 0;
-        struct sockaddr_in name_in;
-
-        ipx2in((const struct sockaddr_ipx *)name, &name_in);
-
-        if (compmode)
-        {
-            name_in.sin_port = htons(CNCNET_PORT);
-        }
-
-        GetAdaptersInfo(NULL, &adapters_size);
-
-        PIP_ADAPTER_INFO adapters = malloc(adapters_size);
-
-        ret = GetAdaptersInfo(adapters, &adapters_size);
-
-        if (ret == ERROR_SUCCESS)
-        {
-            dprintf("  Found %lu adapters\n", (adapters_size / sizeof *adapters));
-            PIP_ADAPTER_INFO adapter = adapters;
-            while (adapter)
+            if (l < recv_buf + recv_buf_len)
             {
-                if (adapter->IpAddressList.IpAddress.String[0])
-                {
-                    ip = inet_addr(adapter->IpAddressList.IpAddress.String);
-                    unsigned int mask = inet_addr(adapter->IpAddressList.IpMask.String);
-                    bcast = (ip & mask) ^ (mask ^ 0xFFFFFFFF);
-
-                    dprintf("    Adapter %lu:\n", adapter->Index);
-                    dprintf("      ip  : %s\n", adapter->IpAddressList.IpAddress.String);
-                    dprintf("      mask: %s\n", adapter->IpAddressList.IpMask.String);
-                    dprintf("      bcast: %s\n", inet_ntoa(*(struct in_addr *)&bcast));
-
-                    break;
-                }
-
-                adapter = adapter->Next;
+                int len = strlen(l);
+                memmove(recv_buf, l, len);
+                recv_buf[len] = '\0';
+                recv_buf_pos = 0;
             }
         }
-
-        dprintf("  binding to %s:%d\n", inet_ntoa(name_in.sin_addr), ntohs(name_in.sin_port));
-        return bind(s, (struct sockaddr *)&name_in, sizeof name_in);
+        else
+        {
+            dprintf("  receive buffer full! discarding it\n");
+            recv_buf_pos = 0;
+        }
     }
-
-    return bind(s, (struct sockaddr *)name, namelen);
-}
-
-int WINAPI ipx_recvfrom(SOCKET s, char *buf, int len, int flags, struct sockaddr_ipx *from, int *fromlen)
-{
-    struct sockaddr_in from_in;
-    int from_in_len = sizeof from_in;
-
-    int ret = recvfrom(s, buf, len, flags, (struct sockaddr *)&from_in, &from_in_len);
-
-    if (compmode && porthack)
-    {
-        from_in.sin_port = htons(CNCNET_PORT);
-    }
-
-    in2ipx(&from_in, from);
-
-    dprintf("recvfrom(s=%d, buf=%p, len=%d, flags=%08X, from=%p, fromlen=%p (%d) -> %d (err: %d)\n", s, buf, len, flags, from, fromlen, *fromlen, ret, WSAGetLastError());
 
     return ret;
 }
 
-int WINAPI ipx_sendto(SOCKET s, const char *buf, int len, int flags, const struct sockaddr_ipx *to, int tolen)
+int WINAPI compat_recvfrom(SOCKET s, char *buf, int len, int flags, struct sockaddr_in *from, int *fromlen)
 {
-    dprintf("sendto(s=%d, buf=%p, len=%d, flags=%08X, to=%p, tolen=%d)\n", s, buf, len, flags, to, tolen);
+    int ret = recvfrom(s, buf, len, flags, (struct sockaddr *)from, fromlen);
 
-    if (to->sa_family == AF_IPX)
+    dprintf("recvfrom(s=%d, buf=%p, len=%d, flags=%08X, from=%p, fromlen=%p (%d) -> %d (err: %d)\n", s, buf, len, flags, from, fromlen, *fromlen, ret, WSAGetLastError());
+
+    if (from->sin_addr.s_addr == tunnel_ip)
     {
-        struct sockaddr_in to_in;
+        from->sin_addr.s_addr = ntohs(from->sin_port);
+        // hack: assuming single port
+        from->sin_port = port;
+        dprintf("  receiving from %s:%d\n", inet_ntoa(from->sin_addr), ntohs(from->sin_port));
+    }
 
-        ipx2in(to, &to_in);
+    return ret;
+}
 
-        if (compmode && to_in.sin_addr.s_addr == bcast)
-        {
-            dprintf("  doing a compatibility broadcast\n");
+int WINAPI compat_sendto(SOCKET s, const char *buf, int len, int flags, const struct sockaddr_in *to, int tolen)
+{
+    struct sockaddr_in tunnel_to;
 
-            for (int i = 0; i < MAX_BCAST; i++)
-            {
-                if (bcast_list[i].sin_family)
-                {
-                    sendto(s, buf, len, flags, (struct sockaddr *)&bcast_list[i], sizeof bcast_list[i]);
-                }
-            }
+    dprintf("sendto(s=%d, buf=%p, len=%d, flags=%08X, to=%p, tolen=%d)\n", s, buf, len, flags, to, tolen);
+    dprintf("  ip: %08X %08X\n", (unsigned int)to->sin_addr.s_addr, (unsigned int)(to->sin_addr.s_addr >> 16));
 
-            return len;
-        }
+    if ((to->sin_addr.s_addr >> 16) == 0 && tunnel_ip)
+    {
+        dprintf("  forwarding to %s:%d\n", inet_ntoa(*(struct in_addr *)&tunnel_ip), (int)to->sin_addr.s_addr);
+        // hack: assuming single port
+        port = to->sin_port;
 
-        return sendto(s, buf, len, flags, (struct sockaddr *)&to_in, sizeof to_in);
+        tunnel_to.sin_family = AF_INET;
+        tunnel_to.sin_port = htons(to->sin_addr.s_addr);
+        tunnel_to.sin_addr.s_addr = tunnel_ip;
+        to = &tunnel_to;
     }
 
     return sendto(s, buf, len, flags, (struct sockaddr *)to, tolen);
 }
 
-int WINAPI ipx_getsockopt(SOCKET s, int level, int optname, char *optval, int *optlen)
-{
-    dprintf("getsockopt(s=%d, level=%08X, optname=%08X, optval=%p, optlen=%p)\n", s, level, optname, optval, optlen);
-
-    if (optname == 0x4007)
-    {
-        return 0;
-    }
-
-    if (level == 0x3E8)
-    {
-        level = 0xFFFF;
-    }
-
-    return getsockopt(s, level, optname, optval, optlen);
-}
-
-int WINAPI ipx_setsockopt(SOCKET s, int level, int optname, const char *optval, int optlen)
-{
-    dprintf("setsockopt(s=%d, level=%08X, optname=%08X, optval=%p, optlen=%d)\n", s, level, optname, optval, optlen);
-
-    if (level == 0x3E8)
-    {
-        level = 0xFFFF;
-    }
-
-    return setsockopt(s, level, optname, optval, optlen);
-}
-
-int WINAPI ipx_getsockname(SOCKET s, struct sockaddr_ipx *name, int *namelen)
-{
-    struct sockaddr_in name_in;
-    int name_in_len = sizeof name_in;
-
-    dprintf("getsockname(s=%d, name=%p, namelen=%p) (%d)\n", s, name, namelen, *namelen);
-
-    int ret = getsockname(s, (struct sockaddr *)&name_in, &name_in_len);
-
-    name_in.sin_addr.s_addr = ip;
-
-    in2ipx(&name_in, name);
-
-    return ret;
-}
